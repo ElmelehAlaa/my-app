ab386125a959f84287d6c11b07f8821d65e2d753
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-09-04 14:57:23 +0200

    09-D1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import ButtonComponent from "./ButtonComponent";
+import ImageComponent from "./ImageComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
+      </header> */}
+      <ButtonComponent button="ciao" />
+      <ImageComponent
+        src="https://www.adesso-online.de/sites/adesso.spotlight-verlag.de/files/styles/mode_content/public/2018-10/ciao_compleanno.jpg"
+        alt="ciao"
+      />
     </div>
   );
 }
---
+++ src/ButtonComponent.jsx
@@ -0,0 +1,6 @@
+const ButtonComponent = props =>(
+    <div>
+        <button>{props.button}</button>
+    </div>
+)
+export default ButtonComponent
